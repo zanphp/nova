24fa5eecb0b6f58de84b089145b4ed8e52de6f1f
chuxiaofeng <chuxiaofeng@youzan.com>
2017-08-08 23:40:59 +0800

    Revert "Merge branch 'revert-cd32be2b' into 'master'"
    This reverts merge request !33

--- src/Protocol/Packer/Abstracts.php
+++ src/Protocol/Packer/Abstracts.php
@@ -11,6 +11,7 @@ use Thrift\Transport\TMemoryBuffer;
 use Thrift\Type\TMessageType;
 use Exception as SysException;
 
+
 abstract class Abstracts
 {
     /**
--- src/Service/ExceptionPacket.php
+++ src/Service/ExceptionPacket.php
@@ -2,12 +2,12 @@
 
 namespace Kdt\Iron\Nova\Service;
 
-use Kdt\Iron\Nova\Exception\RpcException;
 use Kdt\Iron\Nova\Foundation\Traits\InstanceManager;
 use Thrift\Exception\TApplicationException;
 use Thrift\Exception\TException as ThriftException;
 use Exception as SysException;
-use Zan\Framework\Foundation\Exception\ZanException as IronException;
+
+use ZanPHP\Exception\ZanException;
 
 class ExceptionPacket
 {
@@ -38,17 +38,8 @@ class ExceptionPacket
         }
         else
         {
-            return new TApplicationException($e instanceof IronException ? $this->messageInject($e) : $e->getMessage(), $e->getCode());
+            return new TApplicationException($e instanceof ZanException ? $this->messageInject($e) : $e->getMessage(), $e->getCode());
         }
-    }
-
-    /**
-     * @param SysException $e
-     * @return SysException
-     */
-    public function ironExplode(SysException $e)
-    {
-        return new RpcException($e->getMessage(), $e->getCode());
     }
 
     /**
@@ -58,34 +49,5 @@ class ExceptionPacket
     private function messageInject(SysException $e)
     {
         return sprintf('<%s[%s]>%s%s', $this->injectTag, get_class($e), $this->placeTag, $e->getMessage());
-    }
-
-    /**
-     * @param SysException $e
-     * @return array
-     */
-    private function messageExplode(SysException $e)
-    {
-        $message = $e->getMessage();
-        $code = $e->getCode();
-        // pos
-        $headSign = '<'.$this->injectTag.'[';
-        $headLen = strlen($headSign);
-        $headStart = strpos($message, $headSign);
-        $footSign = ']>';
-        $footLen = strlen($footSign);
-        $footStart = strpos($message, $footSign);
-        if (is_numeric($headStart) && is_numeric($footStart))
-        {
-            // cut
-            $exception = substr($message, $headStart + $headLen, $footStart - $headStart - $headLen);
-            $message = substr($message, $footStart + $footLen + strlen($this->placeTag));
-            // over
-            return [$exception, $message, $code];
-        }
-        else
-        {
-            return [null, $message, $code];
-        }
     }
 }
